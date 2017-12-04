1e8b203cd059f21cdf073cb41a69c230311dc5d5
VitalAndreza <andrezaifrn13@gmail.com>
2017-12-04 17:57:36 -0300

    comecei a fazer a tabela gerenciar alimentos

--- bonappetit/core/models.py
+++ bonappetit/core/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Gerenciaralimento(models.Model):
+	codAlimento = models.CharField('CodAlimento ', max_length=50)
+	descricao= models.CharField('Descricao', max_length=100)
+	porcao = models.CharField('Porcao ', max_length=50)
+	preco = models.DecimalField('Preco ', max_digits=5, decimal_places=2)
+
