ad299e277306d712da3fd0231ce5dcffeff5fd38
Elen-Vivian <vivianifrn@gmail.com>
2017-12-05 07:47:42 -0300

    configurações complementares do banco(Rafael)

--- bonappetit/core/admin.py
+++ bonappetit/core/admin.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produto
+admin.site.register(Produto)
+
+from .models import Tipo_produto
+admin.site.register(Tipo_produto)
+
+from .models import Pedido_produto
+admin.site.register(Pedido_produto)
+
+from .models import Pedido
+admin.site.register(Pedido)
+
+from .models import Cliente
+admin.site.register(Cliente)
+
+from .models import Contato
+admin.site.register(Contato)
--- bonappetit/core/models.py
+++ bonappetit/core/models.py
@@ -4,3 +4,45 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Tipo_produto(models.Model):
+	descricao = models.CharField('Descrição', max_length=200)
+
+class Produto(models.Model):
+	descricao = models.CharField('Descrição', max_length=200)
+	nome = models.CharField('Nome', max_length=50)
+	preco = models.CharField('Preço', max_length=4)
+	id_tipoProduto = models.ForeignKey(Tipo_produto, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.nome
+	 
+class Cliente(models.Model):
+	email = models.CharField('Email', max_length=20)
+	senha = models.CharField('Senha', max_length=15)
+	nome = models.CharField('Nome', max_length=50)
+	cep = models.CharField('Cep', max_length=8)
+	logradouro = models.CharField('Logradouro', max_length=30)
+	numero = models.CharField('Numero', max_length=3)
+	admin = models.BooleanField('Administrador')
+	def __str__(self):
+		return self.nome
+
+
+
+class Pedido(models.Model):
+	
+	data = models.DateField(auto_now=True, auto_now_add=False)
+	forma_pagamento = models.CharField('Forma de Pagamento', max_length=14)
+	id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+class Pedido_produto(models.Model):
+	quantidade = models.CharField('Quantidade', max_length=3)
+	id_pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+	id_produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+
+
+
+class Contato(models.Model):
+	numero = models.CharField('Numero', max_length=12)
+	id_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.id_cliente
