6a3588a45c583eb7853017fb141feb513de4f082
Elen-Vivian <vivianifrn@gmail.com>
2017-12-05 09:49:59 -0300

    --

--- bonappetit/bonappetit/urls.py
+++ bonappetit/bonappetit/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	url(r'^$', views.index, name="index"),
+	url(r'^admin/', admin.site.urls),
 ]
+
