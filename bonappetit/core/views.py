9f9c28e64b52d153e9107921c49de8e83bb1a452
Rafael-Soares <rafaelsoaresif@gmail.com>
2017-12-06 15:33:30 -0300

    alterando o banco

--- bonappetit/core/views.py
+++ bonappetit/core/views.py
@@ -5,16 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-	return HttpResponse('Gerenciando ALimentos')
-
-def index(request):
-	return render(request, 'index.html')
-
-
-# Create your views here.
-def index (request):
-	return render(request, 'index.html')
-
-
-def index (request):
 	return render(request, 'index.html')
