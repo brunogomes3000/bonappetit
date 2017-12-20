18feddd2d8ed0ad7b5d86102653af00d59b3cd26
OliAmanda77889 <mandinhaoli123@gmail.com>
2017-12-20 15:32:56 -0300

    atualizando doc

--- bonappetit/core/admin.py
+++ bonappetit/core/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from .models import Produto
- 
+
 class ProdutoAdmin(admin.ModelAdmin):
 	list_display = ['nome', 'descricao', 'preco']
 	search_fields = ['nome']
